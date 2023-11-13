b9242ecbabd17a34fe06c5bfe5c84aa5ddfbc2d9
gabe <gabrieltucker260@gmail.com>
2023-11-13 13:16:14 -0800

    make test1

---
+++ __tests__/agePlanet.test..js
@@ -0,0 +1,17 @@
+
+import { Input } from '../src/js/agePlanet.js'
+
+describe('Input', () => {
+  let person;
+
+  beforeEach(() => {
+    person = new Input(19, 2, 23);
+  });
+
+  test('create a object with Input to get age in earth years', () => {
+    expect(person.age).toEqual(19);
+  });
+
+
+
+});
---
+++ src/js/agePlanet.js
@@ -0,0 +1,7 @@
+export class Input {
+    constructor(age, futureBirthday, pastBirthday) {
+      this.age = age;
+      this.futureBirthday = futureBirthday;
+      this.pastBirthday = pastBirthday;
+    }
+}
